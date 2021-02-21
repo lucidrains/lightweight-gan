87a4bee768f1caa83e650d0d9f8c2f79c7fa50b9
Pawel Paradysz <pa.paradysz@gmail.com>
2021-02-21 16:28:21 +0000

    mighttrainnow

--- cli.py
+++ cli.py
@@ -14,19 +14,24 @@ import torch.distributed as dist
 
 import numpy as np
 
+
 def exists(val):
     return val is not None
+
 
 def default(val, d):
     return val if exists(val) else d
 
+
 def cast_list(el):
     return el if isinstance(el, list) else [el]
 
-def timestamped_filename(prefix = 'generated-'):
+
+def timestamped_filename(prefix='generated-'):
     now = datetime.now()
     timestamp = now.strftime("%m-%d-%Y_%H-%M-%S")
     return f'{prefix}{timestamp}'
+
 
 def set_seed(seed):
     torch.manual_seed(seed)
@@ -35,23 +40,8 @@ def set_seed(seed):
     np.random.seed(seed)
     random.seed(seed)
 
-def run_training(rank, world_size, model_args, data, load_from, new, num_train_steps, name, seed):
-    is_main = rank == 0
-    is_ddp = world_size > 1
 
-    if is_ddp:
-        set_seed(seed)
-        os.environ['MASTER_ADDR'] = 'localhost'
-        os.environ['MASTER_PORT'] = '12355'
-        dist.init_process_group('nccl', rank=rank, world_size=world_size)
-
-        print(f"{rank + 1}/{world_size} process initialized.")
-
-    model_args.update(
-        is_ddp = is_ddp,
-        rank = rank,
-        world_size = world_size
-    )
+def run_training(model_args, dataset, load_from, new, num_train_steps, name, seed):
 
     model = Trainer(**model_args)
 
@@ -60,88 +50,82 @@ def run_training(rank, world_size, model_args, data, load_from, new, num_train_s
     else:
         model.clear()
 
-    model.set_data_src(data)
+    model.set_dataset(dataset)
 
-    for _ in tqdm(range(num_train_steps - model.steps), initial = model.steps, total = num_train_steps, mininterval=10., desc=f'{name}<{data}>'):
+    for i in tqdm(range(num_train_steps - model.steps), initial=model.steps, total=num_train_steps, mininterval=10., desc=f'{name}<{dataset}>'):
         retry_call(model.train, tries=3, exceptions=NanException)
-        if is_main and _ % 50 == 0:
+        if i % 50 == 0:
             model.print_log()
 
     model.save(model.checkpoint_num)
 
-    if is_ddp:
-        dist.destroy_process_group()
 
-def train_from_folder(
-    data = './data',
-    results_dir = './results',
-    models_dir = './models',
-    name = 'default',
-    new = False,
-    load_from = -1,
-    image_size = 256,
-    optimizer = 'adam',
-    fmap_max = 512,
-    transparent = False,
-    greyscale = False,
-    batch_size = 10,
-    gradient_accumulate_every = 4,
-    num_train_steps = 150000,
-    learning_rate = 2e-4,
-    save_every = 1000,
-    evaluate_every = 1000,
-    generate = False,
-    generate_types = ['default', 'ema'],
-    generate_interpolation = False,
-    aug_test = False,
+def train_from_dataset(
+    dataset,
+    results_dir='./results',
+    models_dir='./models',
+    name='default',
+    new=False,
+    load_from=-1,
+    image_size=256,
+    optimizer='adam',
+    fmap_max=512,
+    num_chans=3,
+    batch_size=10,
+    gradient_accumulate_every=4,
+    num_train_steps=150000,
+    learning_rate=2e-4,
+    save_every=1000,
+    evaluate_every=1000,
+    generate=False,
+    generate_types=['default', 'ema'],
+    generate_interpolation=False,
+    aug_test=False,
     aug_prob=None,
     aug_types=['cutout', 'translation'],
     dataset_aug_prob=0.,
-    attn_res_layers = [32],
-    freq_chan_attn = False,
-    disc_output_size = 1,
-    antialias = False,
-    interpolation_num_steps = 100,
-    save_frames = False,
-    num_image_tiles = None,
-    num_workers = None,
-    multi_gpus = False,
-    calculate_fid_every = None,
-    calculate_fid_num_images = 12800,
-    clear_fid_cache = False,
-    seed = 42,
-    amp = False,
-    show_progress = False,
+    attn_res_layers=[32],
+    freq_chan_attn=False,
+    disc_output_size=1,
+    antialias=False,
+    interpolation_num_steps=100,
+    save_frames=False,
+    num_image_tiles=None,
+    num_workers=None,
+    multi_gpus=False,
+    calculate_fid_every=None,
+    calculate_fid_num_images=12800,
+    clear_fid_cache=False,
+    seed=42,
+    show_progress=False,
 ):
     num_image_tiles = default(num_image_tiles, 4 if image_size > 512 else 8)
 
     model_args = dict(
-        name = name,
-        results_dir = results_dir,
-        models_dir = models_dir,
-        batch_size = batch_size,
-        gradient_accumulate_every = gradient_accumulate_every,
-        attn_res_layers = cast_list(attn_res_layers),
-        freq_chan_attn = freq_chan_attn,
-        disc_output_size = disc_output_size,
-        antialias = antialias,
-        image_size = image_size,
-        num_image_tiles = num_image_tiles,
-        optimizer = optimizer,
-        num_workers = num_workers,
-        fmap_max = fmap_max,
-        transparent = transparent,
-        greyscale = greyscale,
-        lr = learning_rate,
-        save_every = save_every,
-        evaluate_every = evaluate_every,
-        aug_prob = aug_prob,
-        aug_types = cast_list(aug_types),
-        dataset_aug_prob = dataset_aug_prob,
-        calculate_fid_every = calculate_fid_every,
-        calculate_fid_num_images = calculate_fid_num_images,
-        clear_fid_cache = clear_fid_cache,
-        amp = amp
+        name=name,
+        results_dir=results_dir,
+        models_dir=models_dir,
+        batch_size=batch_size,
+        gradient_accumulate_every=gradient_accumulate_every,
+        attn_res_layers=cast_list(attn_res_layers),
+        freq_chan_attn=freq_chan_attn,
+        disc_output_size=disc_output_size,
+        antialias=antialias,
+        image_size=image_size,
+        num_image_tiles=num_image_tiles,
+        optimizer=optimizer,
+        num_workers=num_workers,
+        fmap_max=fmap_max,
+        num_chans=num_chans,
+        lr=learning_rate,
+        save_every=save_every,
+        evaluate_every=evaluate_every,
+        aug_prob=aug_prob,
+        aug_types=cast_list(aug_types),
+        dataset_aug_prob=dataset_aug_prob,
+        calculate_fid_every=calculate_fid_every,
+        calculate_fid_num_images=calculate_fid_num_images,
+        clear_fid_cache=clear_fid_cache,
     )
 
     if generate:
@@ -149,7 +133,8 @@ def train_from_folder(
         model.load(load_from)
         samples_name = timestamped_filename()
         checkpoint = model.checkpoint_num
-        dir_result = model.generate(samples_name, num_image_tiles, checkpoint, generate_types)
+        dir_result = model.generate(
+            samples_name, num_image_tiles, checkpoint, generate_types)
         print(f'sample images generated at {dir_result}')
         return
 
@@ -157,8 +142,10 @@ def train_from_folder(
         model = Trainer(**model_args)
         model.load(load_from)
         samples_name = timestamped_filename()
-        model.generate_interpolation(samples_name, num_image_tiles, num_steps = interpolation_num_steps, save_frames = save_frames)
-        print(f'interpolation generated at {results_dir}/{name}/{samples_name}')
+        model.generate_interpolation(
+            samples_name, num_image_tiles, num_steps=interpolation_num_steps, save_frames=save_frames)
+        print(
+            f'interpolation generated at {results_dir}/{name}/{samples_name}')
         return
 
     if show_progress:
@@ -167,19 +154,11 @@ def train_from_folder(
         return
 
     if aug_test:
-        DiffAugmentTest(data=data, image_size=image_size, batch_size=batch_size, types=aug_types, nrow=num_image_tiles)
+        raise NotImplementedError
+        DiffAugmentTest(data=data, image_size=image_size,
+                        batch_size=batch_size, types=aug_types, nrow=num_image_tiles)
         return
 
-    world_size = torch.cuda.device_count()
-
-    if world_size == 1 or not multi_gpus:
-        run_training(0, 1, model_args, data, load_from, new, num_train_steps, name, seed)
-        return
-
-    mp.spawn(run_training,
-        args=(world_size, model_args, data, load_from, new, num_train_steps, name, seed),
-        nprocs=world_size,
-        join=True)
-
-def main():
-    fire.Fire(train_from_folder)
+    run_training(model_args, dataset, load_from,
+                 new, num_train_steps, name, seed)
+    return
